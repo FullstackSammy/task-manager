fa6f5baa9bba4dfdd272bb59c43f7e357e26d81b
Samuel Näsström <nasstromsamuel@icloud.com>
2022-11-15 15:59:33 +0000

    Add functionality for adding tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,11 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
-    let datepicker = document.querySelectorAll('.datepicker');
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll(".datepicker");
     M.Datepicker.init(datepicker, {
         format: "dd mmmm, yyyy",
         i18n: {done: "Select"}
     });
-    let selects = document.querySelectorAll('select');
+
+    // select initialization
+    let selects = document.querySelectorAll("select");
     M.FormSelect.init(selects);
 });
